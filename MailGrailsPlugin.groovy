d1dace1fd0e113bb75460bb3f0a45f2d48ca314b
Marc Palmer <marc@anyware.co.uk>
2008-04-17 19:08:19 +0000

    First commit of empty project

---
+++ MailGrailsPlugin.groovy
@@ -0,0 +1,31 @@
+
+class MailGrailsPlugin {
+    def version = 0.1
+    def dependsOn = [:]
+	
+    def doWithSpring = {
+        // TODO Implement runtime spring config (optional)
+    }
+   
+    def doWithApplicationContext = { applicationContext ->
+        // TODO Implement post initialization spring config (optional)		
+    }
+
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional)
+    }
+	                                      
+    def doWithDynamicMethods = { ctx ->
+        // TODO Implement registering dynamic methods to classes (optional)
+    }
+	
+    def onChange = { event ->
+        // TODO Implement code that is executed when this class plugin class is changed  
+        // the event contains: event.application and event.applicationContext objects
+    }
+                                                                                  
+    def onApplicationChange = { event ->
+        // TODO Implement code that is executed when any class in a GrailsApplication changes
+        // the event contain: event.source, event.application and event.applicationContext objects
+    }
+}
