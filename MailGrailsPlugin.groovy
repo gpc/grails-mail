a28bbb91fb0bf7051b7d703a6d45bb5dea0de5f3
Jeff Brown <jeff@jeffandbetsy.net>
2008-04-18 17:46:15 +0000

    a very naive first step

--- MailGrailsPlugin.groovy
+++ MailGrailsPlugin.groovy
@@ -1,31 +1,52 @@
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSenderImpl
 
 class MailGrailsPlugin {
     def version = 0.1
     def dependsOn = [:]
-	
+    def observe = ["controllers"]
     def doWithSpring = {
         // TODO Implement runtime spring config (optional)
     }
-   
-    def doWithApplicationContext = { applicationContext ->
+
+    def doWithApplicationContext = {applicationContext ->
         // TODO Implement post initialization spring config (optional)		
     }
 
-    def doWithWebDescriptor = { xml ->
+    def doWithWebDescriptor = {xml ->
         // TODO Implement additions to web.xml (optional)
     }
-	                                      
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
+
+    def doWithDynamicMethods = {ctx ->
+        setupSendMethod(application)
     }
-	
-    def onChange = { event ->
-        // TODO Implement code that is executed when this class plugin class is changed  
-        // the event contains: event.application and event.applicationContext objects
+
+    def onChange = {event ->
+        setupSendMethod(application)
     }
-                                                                                  
-    def onApplicationChange = { event ->
+
+    def onApplicationChange = {event ->
         // TODO Implement code that is executed when any class in a GrailsApplication changes
         // the event contain: event.source, event.application and event.applicationContext objects
     }
+
+    private def setupSendMethod(application) {
+        // TODO obviously just a first step...
+        application.controllerClasses.each {controllerClass ->
+            controllerClass.metaClass.send = {cfg, closure ->
+                def mailMessage = new SimpleMailMessage()
+                closure.delegate = mailMessage
+                closure()
+
+                def mailSender = new JavaMailSenderImpl();
+
+                def senderCfg = cfg.sender
+                senderCfg.each {key, val ->
+                    mailSender."${key}" = val
+                }
+                mailSender.send(mailMessage);
+            }
+        }
+    }
+
 }
