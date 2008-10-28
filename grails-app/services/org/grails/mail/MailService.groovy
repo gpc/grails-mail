e079fbdce20f91e0ecfc11e20221d05b78d5b1c0
Peter Ledbrook <peter@cacoethes.co.uk>
2008-10-28 09:38:01 +0000

    Added support for specifying custom mail headers in the DSL.

--- grails-app/services/org/grails/mail/MailService.groovy
+++ grails-app/services/org/grails/mail/MailService.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2005 the original author or authors.
+ * Copyright 2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,23 +16,11 @@
 
 package org.grails.mail
 
-import javax.servlet.http.HttpServletRequest
-import org.springframework.mail.MailSender
-import org.springframework.mail.SimpleMailMessage
 import javax.mail.internet.MimeMessage
 import org.springframework.mail.MailMessage
+import org.springframework.mail.MailSender
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.MimeMailMessage
-import org.codehaus.groovy.grails.commons.ConfigurationHolder
-import org.springframework.web.context.request.RequestContextHolder
-import org.codehaus.groovy.grails.web.pages.GroovyPagesTemplateEngine
-import org.codehaus.groovy.grails.web.servlet.DefaultGrailsApplicationAttributes
-import org.codehaus.groovy.grails.commons.GrailsResourceUtils
-import org.codehaus.groovy.grails.commons.GrailsClassUtils
-import org.codehaus.groovy.grails.plugins.PluginManagerHolder
-import org.springframework.mock.web.*
-import org.springframework.web.context.support.*
-import org.codehaus.groovy.grails.web.context.*
 
 /**
  *
@@ -79,183 +67,4 @@ class MailService {
             }
         }
     }
-}
-
-class MailMessageBuilder {
-    private MailMessage message
-
-    static PATH_TO_MAILVIEWS = "/WEB-INF/grails-app/views"
-    static HTML_CONTENTTYPES = ['text/html', 'text/xhtml']
-
-    MailSender mailSender
-    MailService mailService
-    
-    MailMessageBuilder(MailService svc, MailSender mailSender) {
-        this.mailSender = mailSender
-        this.mailService = svc
-    }
-
-    private MailMessage getMessage() {
-        if(!message) {
-            if(mailSender instanceof JavaMailSender) {
-                message = new MimeMailMessage(mailSender.createMimeMessage() )
-            }
-            else {
-                message = new SimpleMailMessage()
-            }
-            message.from = ConfigurationHolder.config.grails.mail.default.from
-        }
-        return message
-    }
-
-    MailMessage createMessage() { getMessage() }
-
-    void to(String recip) {
-        if(recip) {
-            getMessage().to = [recip] as String[]
-        }
-    }
-    void to(Object[] args) {
-        if(args) {
-            getMessage().to = args as String[]
-        }
-    }
-    void title(title) {
-        subject(title)
-    }
-    void subject(title) {
-        getMessage().subject = title?.toString()
-    }
-    void body(body) {
-        text(body)
-    }
-    void body(Map params) {
-        if (params.view) {
-            // Here need to render it first, establish content type of virtual response / contentType model param
-            renderMailView(params.view, params.model, params.plugin)
-        } else {
-            text(body)
-        }
-    }
-    void text(body) {
-        getMessage().text = body?.toString()
-    }
-    void html(text) {
-        def msg = getMessage()
-        if(msg instanceof MimeMailMessage) {
-            MimeMailMessage mm = msg
-            mm.getMimeMessageHelper().setText(text, true)
-        }
-    }
-    void bcc(String bcc) {
-        getMessage().bcc = [bcc] as String[]
-    }
-    void bcc(Object[] args) {
-        getMessage().bcc = args as String[]
-    }
-    void cc(String cc) {
-        getMessage().cc = [cc] as String[]
-    }
-    void cc(Object[] args) {
-        getMessage().cc = args as String[]
-    }
-    void replyTo(String replyTo) {
-        getMessage().replyTo = replyTo
-    }
-    void from(String from) {
-        getMessage().from = from
-    }
-	
-	protected renderMailView(templateName, model, pluginName = null) {
-        if(!mailService.groovyPagesTemplateEngine) throw new IllegalStateException("Property [groovyPagesTemplateEngine] must be set!")
-        assert templateName
-
-        def engine = mailService.groovyPagesTemplateEngine
-        def requestAttributes = RequestContextHolder.getRequestAttributes()
-		boolean unbindRequest = false
-		
-		// outside of an executing request, establish a mock version
-		if(!requestAttributes) {
-			def servletContext  = ServletContextHolder.getServletContext()
-			def applicationContext = WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext)
-			requestAttributes = grails.util.GrailsWebUtil.bindMockWebRequest(applicationContext)
-			unbindRequest = true
-		}
-		def servletContext = requestAttributes.request.servletContext 
-		def request = requestAttributes.request 
-
-        def grailsAttributes = new DefaultGrailsApplicationAttributes(servletContext);
-        // See if the application has the view for it
-        def uri = getMailViewUri(templateName, request)
-
-        def r = engine.getResourceForUri(uri)
-        // Try plugin view if not found in application
-        if ((!r || !r.exists()) && controllerName) {
-            // Caution, this uses views/ always, whereas our app view resolution uses the PATH_TO_MAILVIEWS which may in future be orthogonal!
-            def plugin = PluginManagerHolder.pluginManager.getGrailsPlugin(pluginName)
-            String pathToView
-            if (plugin) {
-                pathToView = '/plugins/'+plugin.name+'-'+plugin.version+'/'+GrailsResourceUtils.GRAILS_APP_DIR+'/views'+templateName
-            }
-            
-            if (pathToView != null) {
-                uri = GrailsResourceUtils.WEB_INF +pathToView +templateName+".gsp";
-                r = engine.getResourceForUri(uri)
-            }
-        }
-        def t = engine.createTemplate( r )
-
-        def out = new StringWriter();   
-        def originalOut = requestAttributes.getOut()
-        requestAttributes.setOut(out)
-        try {
-            if(model instanceof Map) {
-                t.make( model ).writeTo(out)
-            }
-    		else {
-    			t.make().writeTo(out)
-    		}
-	    } 
-	    finally {
-	        requestAttributes.setOut(originalOut)
-			if(unbindRequest) {
-				RequestContextHolder.setRequestAttributes(null)
-			}
-	    }
-
-	    if (HTML_CONTENTTYPES.contains(t.metaInfo.contentType)) {
-	        html(out.toString()) // @todo Spring mail helper will not set correct mime type if we give it XHTML
-        } else {
-            text(out)
-        }
-    }
-    
-	protected String getMailViewUri(String viewName, HttpServletRequest request) {
-
-        StringBuffer buf = new StringBuffer(PATH_TO_MAILVIEWS);
-	       
-        if(viewName.startsWith("/")) {
-           String tmp = viewName.substring(1,viewName.length());
-           if(tmp.indexOf('/') > -1) {
-        	   buf.append('/');
-        	   buf.append(tmp.substring(0,tmp.lastIndexOf('/')));
-        	   buf.append("/");
-        	   buf.append(tmp.substring(tmp.lastIndexOf('/') + 1,tmp.length()));
-           }
-           else {
-        	   buf.append("/");
-        	   buf.append(viewName.substring(1,viewName.length()));
-           }
-        }
-        else {
-           if (!request) throw new IllegalArgumentException(
-               "Mail views cannot be loaded from relative view paths where there is no current HTTP request")
-           def grailsAttributes = new DefaultGrailsApplicationAttributes(request.servletContext);
-           buf.append(grailsAttributes.getControllerUri(request))
-                .append("/")
-                .append(viewName);
-   
-        }
-        return buf.append(".gsp").toString();
-	}
 }
---
+++ test/unit/org/grails/mail/MailMessageBuilderTests.groovy
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2008 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.grails.mail
+
+import javax.mail.Message
+import javax.mail.Session
+import javax.mail.internet.MimeMessage
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
+import org.springframework.mail.MailSender
+import org.springframework.mail.javamail.JavaMailSender
+
+/**
+ * Test case for {@link MailMessageBuilder}.
+ */
+class MailMessageBuilderTests extends GroovyTestCase {
+    def mockService
+    def mockSender
+    def testBuilder
+
+    void setUp() {
+        super.setUp()
+
+        ConfigurationHolder.config = new ConfigObject()
+        mockService = [:] as MailService
+        mockSender = [
+                createMimeMessage: {-> return new MimeMessage(Session.getInstance(new Properties())) }
+        ] as JavaMailSender
+        testBuilder = new MailMessageBuilder(mockService, mockSender)
+    }
+
+    void tearDown() {
+        ConfigurationHolder.config = null
+        super.tearDown()
+    }
+
+    /**
+     * Tests the basic elements of the mail DSL.
+     */
+    void testBasics() {
+        processDsl {
+            to "fred@g2one.com"
+            subject "Hello Fred"
+            body 'How are you?'
+        }
+
+        def msg = testBuilder.createMessage().mimeMessage
+        assertEquals 1, to(msg).size()
+        assertEquals "fred@g2one.com", to(msg)[0].toString()
+        assertEquals "Hello Fred", msg.subject
+        assertEquals "How are you?", msg.content
+    }
+
+    /**
+     * Tests that multiple recipients are added to the underlying mail
+     * message correctly.
+     */
+    void testMultipleRecipients() {
+        processDsl {
+            to "fred@g2one.com","ginger@g2one.com", "grace@hollywood.com"
+            from "john@g2one.com"
+            cc "marge@g2one.com", "ed@g2one.com"
+            bcc "joe@g2one.com"
+            subject "Hello John"
+            body 'this is some text'
+        }
+
+        def msg = testBuilder.createMessage().mimeMessage
+        assertEquals([ "fred@g2one.com", "ginger@g2one.com", "grace@hollywood.com" ], to(msg))
+        assertEquals([ "marge@g2one.com", "ed@g2one.com" ], cc(msg))
+        assertEquals([ "joe@g2one.com" ], bcc(msg))
+        assertEquals 1, msg.from.size()
+        assertEquals "john@g2one.com", msg.from[0].toString()
+        assertEquals "Hello John", msg.subject
+        assertEquals "this is some text", msg.content
+    }
+
+    /**
+     * Tests the "headers" feature of the mail DSL. It should add the
+     * specified headers to the underlying MIME message.
+     */
+    void testHeaders() {
+        processDsl {
+            headers "X-Mailing-List": "user@grails.codehaus.org",
+                    "Sender": "dilbert@somewhere.org"
+            to "fred@g2one.com"
+            subject "Hello Fred"
+            body 'How are you?'
+        }
+
+        def msg = testBuilder.createMessage().mimeMessage
+        assertEquals "user@grails.codehaus.org", msg.getHeader("X-Mailing-List", ", ")
+        assertEquals "dilbert@somewhere.org", msg.getHeader("Sender", ", ")
+        assertEquals([ "fred@g2one.com" ], to(msg))
+        assertEquals "Hello Fred", msg.subject
+        assertEquals "How are you?", msg.content
+    }
+
+    /**
+     * Tests that the builder throws an exception if the user tries to
+     * specify custom headers with just a plain MailSender.
+     */
+    void testHeadersWithBasicMailSender() {
+        mockSender = [:] as MailSender
+        testBuilder = new MailMessageBuilder(mockService, mockSender)
+
+        shouldFail(GrailsMailException) {
+            processDsl {
+                headers "Content-Type": "text/plain;charset=UTF-8",
+                        "Sender": "dilbert@somewhere.org"
+                to "fred@g2one.com"
+                subject "Hello Fred"
+                body 'How are you?'
+            }
+        }
+    }
+
+    private List to(MimeMessage msg) {
+        msg.getRecipients(Message.RecipientType.TO)*.toString()
+    }
+
+    private List cc(MimeMessage msg) {
+        msg.getRecipients(Message.RecipientType.CC)*.toString()
+    }
+
+    private List bcc(MimeMessage msg) {
+        msg.getRecipients(Message.RecipientType.BCC)*.toString()
+    }
+
+    private processDsl(Closure c) {
+        c.delegate = this.testBuilder
+        c.call()
+    }
+}
