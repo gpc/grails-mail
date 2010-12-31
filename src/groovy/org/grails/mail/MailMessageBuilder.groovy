5a2e62d6b61e88e04c5a20df377ff08f5c0cc97d
Luke Daley <ld@ldaley.com>
2010-12-31 12:49:27 +1000

    Refactored the rendering of GSPs out into a separate class to more clearly separate concerns.

--- src/groovy/org/grails/mail/MailMessageBuilder.groovy
+++ src/groovy/org/grails/mail/MailMessageBuilder.groovy
@@ -15,33 +15,21 @@
  */
 package org.grails.mail
 
-import grails.util.GrailsWebUtil
-import javax.servlet.http.HttpServletRequest
-import org.codehaus.groovy.grails.commons.GrailsResourceUtils
-import org.codehaus.groovy.grails.plugins.PluginManagerHolder
-import org.codehaus.groovy.grails.web.context.ServletContextHolder
-import org.codehaus.groovy.grails.web.servlet.DefaultGrailsApplicationAttributes
 import org.springframework.mail.MailMessage
 import org.springframework.mail.MailSender
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.*
 import org.springframework.core.io.*
-import org.springframework.web.context.request.RequestContextHolder
-import org.springframework.web.context.support.WebApplicationContextUtils
-import org.codehaus.groovy.grails.commons.GrailsClassUtils as GCU
+
 import org.apache.commons.logging.LogFactory
-import org.springframework.web.servlet.i18n.FixedLocaleResolver
-import org.springframework.web.servlet.DispatcherServlet
 
 /**
  * The builder that implements the mail DSL.
  */
 class MailMessageBuilder {
+
     private MailMessage message
     private Locale locale
-
-    static PATH_TO_MAILVIEWS = "/WEB-INF/grails-app/views"
-    static HTML_CONTENTTYPES = ['text/html', 'text/xhtml']
 
     private log = LogFactory.getLog(MailMessageBuilder)
     
@@ -140,9 +128,20 @@ class MailMessageBuilder {
     void body(Map params) {
         if (params.view) {
             // Here need to render it first, establish content type of virtual response / contentType model param
-            renderMailView(params.view, params.model, params.plugin)
+            def render = createContentRenderer().render(new StringWriter(), params.view, params.model, locale, params.plugin)
+            
+            if (render.html) {
+                html(render.out.toString()) // @todo Spring mail helper will not set correct mime type if we give it XHTML
+            } else {
+                text(render.out.toString())
+            }
         } 
     }
+    
+    protected createContentRenderer() {
+        new MailMessageContentRenderer(mailService)
+    }
+    
     void text(body) {
         getMessage().text = body?.toString()
     }
@@ -210,111 +209,5 @@ class MailMessageBuilder {
     void locale(Locale locale) {
         this.locale=locale
     }
-
-
-    protected renderMailView(templateName, model, pluginName = null) {
-        if(!mailService.groovyPagesTemplateEngine) throw new IllegalStateException("Property [groovyPagesTemplateEngine] must be set!")
-        assert templateName
-
-        def engine = mailService.groovyPagesTemplateEngine
-        def requestAttributes = RequestContextHolder.getRequestAttributes()
-        boolean unbindRequest = false
-
-        // outside of an executing request, establish a mock version
-        if(!requestAttributes) {
-            def servletContext  = ServletContextHolder.getServletContext()
-            def applicationContext = WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext)
-            requestAttributes = GrailsWebUtil.bindMockWebRequest(applicationContext)
-            unbindRequest = true
-        }
-        def servletContext = requestAttributes.request.servletContext
-        def request = requestAttributes.request
-        if (locale) {
-            request.setAttribute(DispatcherServlet.LOCALE_RESOLVER_ATTRIBUTE,new FixedLocaleResolver(defaultLocale:locale))
-        }
-
-        def grailsAttributes = new DefaultGrailsApplicationAttributes(servletContext);
-        // See if the application has the view for it
-        def uri = getMailViewUri(templateName, request)
-
-        def r = engine.getResourceForUri(uri)
-        // Try plugin view if not found in application
-        if (!r || !r.exists()) {
-            if (log.debugEnabled) {
-                log.debug "Could not locate email view ${templateName} at ${uri}, trying plugin"
-            }
-            if (pluginName) {
-                // Caution, this uses views/ always, whereas our app view resolution uses the PATH_TO_MAILVIEWS which may in future be orthogonal!
-                def plugin = PluginManagerHolder.pluginManager.getGrailsPlugin(pluginName)
-                String pathToView = null
-                if (plugin) {
-                    pathToView = '/plugins/'+GCU.getScriptName(plugin.name)+'-'+plugin.version+'/'+GrailsResourceUtils.GRAILS_APP_DIR+'/views'
-                }
-
-                if (pathToView != null) {
-                    uri = GrailsResourceUtils.WEB_INF +pathToView +templateName+".gsp";
-                    r = engine.getResourceForUri(uri)
-                } else {
-                    if (log.errorEnabled) {
-                        log.error "Could not locate email view ${templateName} in plugin [$pluginName]"
-                    }
-                    throw new IllegalArgumentException("Could not locate email view ${templateName} in plugin [$pluginName]")
-                }
-            } else {
-                if (log.errorEnabled) {
-                    log.error "Could not locate email view ${templateName} at ${uri}, no pluginName specified so couldn't look there"
-                }
-                throw new IllegalArgumentException("Could not locate mail body ${templateName}. Is it in a plugin? If so you must pass the plugin name in the [plugin] variable")
-            }
-        }
-        def t = engine.createTemplate( r )
-
-        def out = new StringWriter();
-        def originalOut = requestAttributes.getOut()
-        requestAttributes.setOut(out)
-        try {
-            if(model instanceof Map) {
-                t.make( model ).writeTo(out)
-            }
-            else {
-                t.make().writeTo(out)
-            }
-        }
-        finally {
-            requestAttributes.setOut(originalOut)
-            if(unbindRequest) {
-                RequestContextHolder.setRequestAttributes(null)
-            }
-        }
-
-        if (HTML_CONTENTTYPES.contains(t.metaInfo.contentType)) {
-            html(out.toString()) // @todo Spring mail helper will not set correct mime type if we give it XHTML
-        } else {
-            text(out)
-        }
-    }
-
-    protected String getMailViewUri(String viewName, HttpServletRequest request) {
-
-        def buf = new StringBuilder(PATH_TO_MAILVIEWS)
-        
-        if(viewName.startsWith("/")) {
-           def tmp = viewName[1..-1]
-           if(tmp.indexOf('/') > -1) {
-               def i = tmp.lastIndexOf('/')
-               buf << "/${tmp[0..i]}/${tmp[(i+1)..-1]}"
-           }
-           else {
-               buf << "/${viewName[1..-1]}"
-           }
-        }
-        else {
-           if (!request) throw new IllegalArgumentException(
-               "Mail views cannot be loaded from relative view paths where there is no current HTTP request")
-           def grailsAttributes = new DefaultGrailsApplicationAttributes(request.servletContext)
-           buf << "${grailsAttributes.getControllerUri(request)}/${viewName}"
-
-        }
-        return buf.append(".gsp").toString()
-    }
+    
 }
---
+++ src/groovy/org/grails/mail/MailMessageContentRenderer.groovy
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.grails.mail
+
+import grails.util.GrailsWebUtil
+
+import org.codehaus.groovy.grails.web.context.ServletContextHolder
+import org.codehaus.groovy.grails.web.servlet.DefaultGrailsApplicationAttributes
+import org.codehaus.groovy.grails.plugins.PluginManagerHolder
+import org.codehaus.groovy.grails.commons.GrailsResourceUtils
+import org.codehaus.groovy.grails.commons.GrailsClassUtils as GCU
+
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.support.WebApplicationContextUtils
+import org.springframework.web.servlet.DispatcherServlet
+import org.springframework.web.servlet.i18n.FixedLocaleResolver
+
+import org.apache.commons.logging.LogFactory
+
+import javax.servlet.http.HttpServletRequest
+
+/**
+ * Responsible for rendering a GSP into the content of a mail message.
+ */
+class MailMessageContentRenderer {
+
+    static PATH_TO_MAILVIEWS = "/WEB-INF/grails-app/views"
+    
+    
+    private log = LogFactory.getLog(MailMessageContentRenderer)
+    
+    def mailService
+    
+    MailMessageContentRenderer(mailService) {
+        this.mailService = mailService
+    }
+    
+    MailMessageContentRender render(Writer out, templateName, model, locale, pluginName = null) {
+        if(!mailService.groovyPagesTemplateEngine) throw new IllegalStateException("Property [groovyPagesTemplateEngine] must be set!")
+        assert templateName
+
+        def engine = mailService.groovyPagesTemplateEngine
+        def requestAttributes = RequestContextHolder.getRequestAttributes()
+        boolean unbindRequest = false
+
+        // outside of an executing request, establish a mock version
+        if(!requestAttributes) {
+            def servletContext  = ServletContextHolder.getServletContext()
+            def applicationContext = WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext)
+            requestAttributes = GrailsWebUtil.bindMockWebRequest(applicationContext)
+            unbindRequest = true
+        }
+        def servletContext = requestAttributes.request.servletContext
+        def request = requestAttributes.request
+        if (locale) {
+            request.setAttribute(DispatcherServlet.LOCALE_RESOLVER_ATTRIBUTE,new FixedLocaleResolver(defaultLocale:locale))
+        }
+
+        def grailsAttributes = new DefaultGrailsApplicationAttributes(servletContext);
+        // See if the application has the view for it
+        def uri = getMailViewUri(templateName, request)
+
+        def r = engine.getResourceForUri(uri)
+        // Try plugin view if not found in application
+        if (!r || !r.exists()) {
+            if (log.debugEnabled) {
+                log.debug "Could not locate email view ${templateName} at ${uri}, trying plugin"
+            }
+            if (pluginName) {
+                // Caution, this uses views/ always, whereas our app view resolution uses the PATH_TO_MAILVIEWS which may in future be orthogonal!
+                def plugin = PluginManagerHolder.pluginManager.getGrailsPlugin(pluginName)
+                String pathToView = null
+                if (plugin) {
+                    pathToView = '/plugins/'+GCU.getScriptName(plugin.name)+'-'+plugin.version+'/'+GrailsResourceUtils.GRAILS_APP_DIR+'/views'
+                }
+
+                if (pathToView != null) {
+                    uri = GrailsResourceUtils.WEB_INF +pathToView +templateName+".gsp";
+                    r = engine.getResourceForUri(uri)
+                } else {
+                    if (log.errorEnabled) {
+                        log.error "Could not locate email view ${templateName} in plugin [$pluginName]"
+                    }
+                    throw new IllegalArgumentException("Could not locate email view ${templateName} in plugin [$pluginName]")
+                }
+            } else {
+                if (log.errorEnabled) {
+                    log.error "Could not locate email view ${templateName} at ${uri}, no pluginName specified so couldn't look there"
+                }
+                throw new IllegalArgumentException("Could not locate mail body ${templateName}. Is it in a plugin? If so you must pass the plugin name in the [plugin] variable")
+            }
+        }
+        def t = engine.createTemplate( r )
+
+        def originalOut = requestAttributes.getOut()
+        requestAttributes.setOut(out)
+        try {
+            if(model instanceof Map) {
+                t.make( model ).writeTo(out)
+            }
+            else {
+                t.make().writeTo(out)
+            }
+        }
+        finally {
+            requestAttributes.setOut(originalOut)
+            if(unbindRequest) {
+                RequestContextHolder.setRequestAttributes(null)
+            }
+        }
+
+        new MailMessageContentRender(out, t.metaInfo.contentType)
+    }
+
+    protected String getMailViewUri(String viewName, HttpServletRequest request) {
+
+        def buf = new StringBuilder(PATH_TO_MAILVIEWS)
+        
+        if(viewName.startsWith("/")) {
+           def tmp = viewName[1..-1]
+           if(tmp.indexOf('/') > -1) {
+               def i = tmp.lastIndexOf('/')
+               buf << "/${tmp[0..i]}/${tmp[(i+1)..-1]}"
+           }
+           else {
+               buf << "/${viewName[1..-1]}"
+           }
+        }
+        else {
+           if (!request) throw new IllegalArgumentException(
+               "Mail views cannot be loaded from relative view paths where there is no current HTTP request")
+           def grailsAttributes = new DefaultGrailsApplicationAttributes(request.servletContext)
+           buf << "${grailsAttributes.getControllerUri(request)}/${viewName}"
+
+        }
+        return buf.append(".gsp").toString()
+    }
+    
+    
+}
