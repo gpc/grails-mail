9cb7e3c97a029cec00a8fede976680d01f6cf2fd
Luke Daley <ld@ldaley.com>
2011-01-04 11:49:15 +1000

    Added extra docs info.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -3,3 +3,13 @@ grails.mail.default.from = "for@testing.com"
 log4j = {
     trace 'grails.plugin.mail'
 }
+
+grails {
+    doc {
+        title = "Grails Mail Plugin"
+        subtitle = "Send email from Grails applications"
+        authors = "Grails Plugin Collective"
+        copyright = "Copies of this document may be made for your own use and for distribution to others, provided that you do not charge any fee for such copies and further provided that each copy contains this Copyright Notice, whether distributed in print or electronically."
+        footer = "Developed by the <a href='http://gpc.github.com'>Grails Plugin Collective</a>"
+    }
+}
