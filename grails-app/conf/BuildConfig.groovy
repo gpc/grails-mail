90ed98c79078aaefcf2a7c8dfef602e887ad4713
Marc Palmer <marc@anyware.co.uk>
2011-12-20 15:19:58 +0000

    Updated with more Grails versions' spring release numbers and new mech from Luke for post-Grails 2.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,10 +1,19 @@
 grails.project.work.dir = "target"
 
+// Get the correct spring version
 def springTestDepsByGrailsVersion = [
+    '1.3.0':':org.springframework.test:3.0.0.RELEASE',
+    '1.3.1':':org.springframework.test:3.0.2.RELEASE',
+    '1.3.2':':org.springframework.test:3.0.3.RELEASE',
+    '1.3.3':':org.springframework.test:3.0.3.RELEASE',
+    '1.3.4':':org.springframework.test:3.0.3.RELEASE',
+    '1.3.5':':org.springframework.test:3.0.3.RELEASE',
+    '1.3.6':':org.springframework.test:3.0.5.RELEASE',
     '1.3.7':':org.springframework.test:3.0.5.RELEASE',
     '2.0':'org.springframework:spring-test:3.1.0.RELEASE'
 ]
-def springTestDep = springTestDepsByGrailsVersion[grailsVersion]
+// For Grails version newer than 2.0 we can find this out, so fall back to that
+def springTestDep = springTestDepsByGrailsVersion[grailsVersion] ?: "org.springframework:spring-test:$grailsCoreDependencies.springVersion"
 
 grails.project.dependency.resolution = {
     inherits("global")
