088970b3e7c94484604aefd1df17a9f7105ff6e1
Luke Daley <ld@ldaley.com>
2011-12-21 13:38:54 +0000

    Always use spring-test 3.1.0
    We can't use any conditional logic because that can't be expressed in POM files. This is the best we can do.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,30 +1,5 @@
 grails.project.work.dir = "target"
 
-// Get the correct spring version
-def springTestDepsByGrailsVersion = [
-    '1.3.0':':org.springframework.test:3.0.0.RELEASE',
-    '1.3.1':':org.springframework.test:3.0.2.RELEASE',
-    '1.3.2':':org.springframework.test:3.0.3.RELEASE',
-    '1.3.3':':org.springframework.test:3.0.3.RELEASE',
-    '1.3.4':':org.springframework.test:3.0.3.RELEASE',
-    '1.3.5':':org.springframework.test:3.0.3.RELEASE',
-    '1.3.6':':org.springframework.test:3.0.5.RELEASE',
-    '1.3.7':':org.springframework.test:3.0.5.RELEASE',
-    '2.0':'org.springframework:spring-test:3.1.0.RELEASE'
-]
-// For Grails version newer than 2.0 we can find this out, so fall back to that
-def springTestDep = springTestDepsByGrailsVersion[grailsVersion]
-
-if (springTestDep == null) {
-    if (grailsSettings.hasProperty("coreDependencies") && grailsSettings.coreDependencies.hasProperty("springVersion")) {
-        // We are on Grails 2.1+
-        springTestDep = "org.springframework:spring-test:$grailsSettings.coreDependencies.springVersion"
-    } else {
-        // we don't know the spring version, guess at it
-        springTestDep = 'org.springframework:spring-test:3.1.0.RELEASE'
-    }
-}
-
 grails.project.dependency.resolution = {
     inherits("global")
     repositories {
@@ -34,13 +9,16 @@ grails.project.dependency.resolution = {
     dependencies {
         compile "javax.mail:mail:1.4.3"
         
-        runtime(springTestDep) {
+        runtime("org.springframework:spring-test:3.1.0.RELEASE") {
             transitive = false
         }
     }
     plugins {
         if (appName == "grails-mail") {
             test (":greenmail:1.3.0") {
+                export = false
+            }
+            build(":release:1.0.0") {
                 export = false
             }
         }
@@ -51,3 +29,5 @@ if (appName == "grails-mail") {
     // use for testing view resolution from plugins
     grails.plugin.location.'for-plugin-view-resolution' = 'plugins/for-plugin-view-resolution'
 }
+
+grails.release.scm.enabled = false
