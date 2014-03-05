cef1bcf1102a5c69e9e47b54bc77ca20176800c2
Craig Andrews <candrews@integralblue.com>
2014-03-05 13:31:21 -0500

    Upgrade to Grails 2.2.5

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -25,7 +25,7 @@ grails.project.dependency.resolution = {
         test (":greenmail:1.3.0") {
             export = false
         }
-        build(":release:2.0.4", ":rest-client-builder:1.0.2") {
+        build ':release:2.2.1', ':rest-client-builder:1.0.3', {
             export = false
         }
     }
