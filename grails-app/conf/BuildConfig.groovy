a730c2bef0f4adcef70882a397154ce2eaf8c728
Craig Andrews <candrews@integralblue.com>
2014-03-05 12:02:09 -0500

    Set the grailsCentral credentials based on the environment variables

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,3 +1,6 @@
+grails.project.repos.grailsCentral.username = System.getenv("GRAILS_CENTRAL_USERNAME")
+grails.project.repos.grailsCentral.password = System.getenv("GRAILS_CENTRAL_PASSWORD")
+
 grails.project.work.dir = "target"
 
 grails.project.dependency.resolution = {
