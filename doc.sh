73e61bd28520f5496ed7aa2b65c4535750c95322
Craig Andrews <candrews@integralblue.com>
2014-03-06 18:25:24 -0500

    Fixes

--- doc.sh
+++ doc.sh
@@ -8,11 +8,11 @@
 
 rm -rf docs &&
 ./grailsw doc --pdf &&
-git config user.name "$GIT_NAME" &&
-git config user.email "$GIT_EMAIL" &&
-git config credential.helper "store --file=.git/credentials" &&
-echo "https://$GH_TOKEN:@github.com" > .git/credentials &&
-git clone https://github.com/$TRAVIS_REPO_SLUG.git -b gh-pages gh-pages --single-branch &&
+git config --global user.name "$GIT_NAME" &&
+git config --global user.email "$GIT_EMAIL" &&
+git config --global credential.helper "store --file=~/.git-credentials" &&
+echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials &&
+git clone https://${GH_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git -b gh-pages gh-pages --single-branch > /dev/null &&
 cd gh-pages &&
 git rm -rf . &&
 cp -r ../docs . &&
