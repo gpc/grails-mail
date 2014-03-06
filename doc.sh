a8c4d2f050af00a29c0e66768fafe2b6269ce78d
Craig Andrews <candrews@integralblue.com>
2014-03-06 18:48:32 -0500

    Correctly use the standard Grails doc location

--- doc.sh
+++ doc.sh
@@ -6,7 +6,6 @@
 # GIT_EMAIL
 # GIT_TOKEN
 
-rm -rf docs &&
 ./grailsw doc --pdf &&
 git config --global user.name "$GIT_NAME" &&
 git config --global user.email "$GIT_EMAIL" &&
@@ -15,7 +14,7 @@ echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials &&
 git clone https://${GH_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git -b gh-pages gh-pages --single-branch > /dev/null &&
 cd gh-pages &&
 git rm -rf . &&
-cp -r ../docs . &&
+cp -r ../target/docs/. ./ &&
 git add * &&
 git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID" &&
 git push origin HEAD &&
