01cf90b4e8decd9f1cba8952324e5b4de39c0aa3
Craig Andrews <candrews@integralblue.com>
2014-03-06 19:17:10 -0500

    Always generate the docs

--- doc.sh
+++ doc.sh
@@ -6,7 +6,6 @@
 # GIT_EMAIL
 # GIT_TOKEN
 
-./grailsw doc --pdf &&
 git config --global user.name "$GIT_NAME" &&
 git config --global user.email "$GIT_EMAIL" &&
 git config --global credential.helper "store --file=~/.git-credentials" &&
