fec9a9c8c6fd91f5d1786651705c962efae9116b
Craig Andrews <candrews@integralblue.com>
2014-03-07 12:10:42 -0500

    Explicitly list spring-test dependency exclusions
    Grails 2.3 and higher can use Aether/Maven instead of Ivy which does not support "transitive = false" so explicitly list the exclusions

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -17,8 +17,40 @@ grails.project.dependency.resolution = {
 		compile "javax.mail:javax.mail-api:1.5.1"
         runtime "com.sun.mail:javax.mail:1.5.1"
 
-        runtime("org.springframework:spring-test:3.1.0.RELEASE") {
-            transitive = false
+        compile("org.springframework:spring-test:3.1.0.RELEASE") {
+            // Grails 2.3 and higher can use Aether/Maven instead of Ivy
+            // which does not support:
+            // transitive = false
+            // so explicitly list the exclusions
+            excludes 'com.jayway.jsonpath:json-path',
+                'hsqldb:hsqldb',
+                'javax.activation:activation',
+                'javax.el:el-api',
+                'javax.inject:javax.inject',
+                'javax.persistence:persistence-api',
+                'javax.portlet:portlet-api',
+                'javax.servlet.jsp:jsp-api',
+                'javax.servlet:javax.servlet-api',
+                'javax.servlet:jstl',
+                'junit:junit',
+                'org.apache.geronimo.specs:geronimo-jta_1.1_spec',
+                'org.hibernate:hibernate-cglib-repack',
+                'org.hibernate:hibernate-core',
+                'org.aspectj:aspectjweaver',
+                'org.eclipse.persistence:javax.persistence',
+                'org.hamcrest:hamcrest-core',
+                'org.springframework:spring-beans',
+                'org.springframework:spring-context',
+                'org.springframework:spring-core',
+                'org.springframework:spring-jdbc',
+                'org.springframework:spring-orm',
+                'org.springframework:spring-tx',
+                'org.springframework:spring-web',
+                'org.springframework:spring-webmvc',
+                'org.springframework:spring-webmvc-portlet',
+                'org.testng:testng',
+                'xmlunit:xmlunit',
+                'taglibs:standard'
         }
     }
 
